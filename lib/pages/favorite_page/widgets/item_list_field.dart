627f6b64313fb1cc31aae27a0c181d90eed192f3
izzetJmy <127131653+izzetJmy@users.noreply.github.com>
2023-12-24 17:11:17 +0300

    fix

--- lib/pages/favorite_page/widgets/item_list_field.dart
+++ lib/pages/favorite_page/widgets/item_list_field.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mylibrary/model/card_model.dart';
-import 'package:mylibrary/providers/all_providers.dart';
 import 'package:mylibrary/widgets/item_card.dart';
 
 // ignore: must_be_immutable
@@ -26,115 +25,44 @@ class _ItemListFavoriteFieldState extends ConsumerState<ItemListFavoriteField> {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(
-          horizontal: widget.height * 0.018, vertical: widget.height * 0.002),
+          horizontal: height * 0.018, vertical: height * 0.002),
       margin: const EdgeInsets.fromLTRB(21, 0, 21, 0),
-      height: widget.height * 0.67,
+      height: height * 0.62,
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.primary,
         borderRadius: BorderRadius.circular(15),
         border: Border.all(
-            color: Theme.of(context).colorScheme.tertiary, width: 1.3),
+            color: Theme.of(context).colorScheme.tertiary, width: 1.2),
       ),
       child: StreamBuilder(
-        stream: controlStramCard(ref, widget.searchController),
+        stream: searchController.text.isEmpty
+            ? favoriteCardsStream
+            : searchCardsStream,
         builder: (context, snapshot) {
           if (snapshot.hasError) {
-            return const Center(
-              child: Text('errrorr'),
-            );
+            return Text(snapshot.error.toString());
           }
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
+            return Container(
+              padding: EdgeInsets.symmetric(
+                  horizontal: height * 0.018, vertical: height * 0.002),
+              margin: const EdgeInsets.fromLTRB(21, 0, 21, 0),
+              height: height * 0.62,
             );
           } else {
             return ListView.builder(
-              shrinkWrap: true,
               itemCount: snapshot.data!.docs.length,
               itemBuilder: (context, index) {
-                var data = snapshot.data!.docs
+                List<Map<String, dynamic>> data = snapshot.data!.docs
                     .map((DocumentSnapshot documentSnapshot) =>
                         documentSnapshot.data() as Map<String, dynamic>)
                     .toList();
-                final item =
-                    ItemCard(cardModel: CardModel.fromJson(data[index]));
-                return Dismissible(
-                  key: ObjectKey(item),
-                  background: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(20)),
-                    alignment: Alignment.centerRight,
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Icon(
-                      Icons.delete_forever,
-                      color: Theme.of(context).colorScheme.primary,
-                      size: 30,
-                    ),
-                  ),
-                  behavior: HitTestBehavior.translucent,
-                  direction: DismissDirection.endToStart,
-                  onDismissed: (direction) {
-                    ref.watch(fireStoreServiceProvider).deleteCard(
-                          CardModel.fromJson(
-                            data[index],
-                          ),
-                        );
-                    if (direction == DismissDirection.endToStart) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Kart silindi!!!'),
-                          duration: Duration(seconds: 1),
-                          dismissDirection: DismissDirection.up,
-                          behavior: SnackBarBehavior.floating,
-                        ),
-                      );
-                    }
-                  },
-                  child: ItemCard(
-                    cardModel: CardModel.fromJson(
-                      data[index],
-                    ),
-                  ),
-                );
+                return ItemCard(cardModel: CardModel.fromJson(data[index]));
               },
             );
           }
         },
       ),
     );
-  }
-}
-
-Stream<QuerySnapshot<Map<String, dynamic>>> controlStramCard(
-    WidgetRef ref, TextEditingController searchController) {
-  List typeList = ref.watch(itemTypeValueListProvider).typeListFavorite;
-  List itemRateList = ref.watch(itemRateValueListProvider).itemRateListFavorite;
-  debugPrint(searchController.toString());
-  final cardsStream =
-      ref.watch(fireStoreServiceProvider.notifier).getFavoriteList();
-
-  Stream<QuerySnapshot<Map<String, dynamic>>> searchFavoriteStream =
-      ref.watch(fireStoreServiceProvider).searchFavoriteCardStream;
-
-  Stream<QuerySnapshot<Map<String, dynamic>>> filterFavoriteCardStream =
-      ref.watch(fireStoreServiceProvider).filterFavoriteCardStream;
-
-  Stream<QuerySnapshot<Map<String, dynamic>>> itemRateFavoriteCardStream =
-      ref.watch(fireStoreServiceProvider).itemRateFavoriteCardStream;
-
-  if (searchController.text.isEmpty &&
-      List.from(typeList).every((element) => element == false) &&
-      List.from(itemRateList).every((element) => element == false)) {
-    return cardsStream;
-  } else if (searchController.text.isNotEmpty &&
-      List.from(typeList).every((element) => element == false) &&
-      List.from(itemRateList).every((element) => element == false)) {
-    return searchFavoriteStream;
-  } else if (List.from(typeList).any((element) => true) &&
-      List.from(itemRateList).every((element) => element == false)) {
-    return filterFavoriteCardStream;
-  } else {
-    return itemRateFavoriteCardStream;
   }
 }
